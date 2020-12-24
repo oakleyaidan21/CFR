22786a69755320c879adb7c3626075ae94b6c1df
Aidan Oakley <oaklea@Aidans-MBP.home>
2020-12-24 17:41:39 -0500

    fixed loading

--- components/ImageWithIndicator.tsx
+++ components/ImageWithIndicator.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react";
-import { Animated, StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions, ActivityIndicator } from "react-native";
+import { Bar } from "react-native-progress";
 import FastImage from "react-native-fast-image";
 
 type Props = {
@@ -8,48 +9,32 @@ type Props = {
   resizeMode: any;
 };
 
-const ImageWithIndicator: React.FC<Props> = (props) => {
-  const progressAnimation = useRef(new Animated.Value(0)).current;
-  const [showAnimation, setShowAnimation] = useState(false);
+const WINDOW_WIDTH = Dimensions.get("window").width;
 
-  const progress = (value: number) => {
-    Animated.timing(progressAnimation, {
-      toValue: value,
-      duration: 100,
-      useNativeDriver: true,
-    }).start();
+const ImageWithIndicator: React.FC<Props> = (props) => {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+  const onLoadEnd = () => {
+    setLoading(false);
   };
 
   const onProgress = (e: any) => {
-    progress(e.nativeEvent.loaded / e.nativeEvent.total);
+    setProgress(e.nativeEvent.loaded / e.nativeEvent.total);
   };
-
-  const onLoadStart = () => {
-    setShowAnimation(true);
-  };
-
-  const onLoadEnd = () => {
-    setShowAnimation(false);
-  };
-
-  const width = progressAnimation.interpolate({
-    inputRange: [0.0, 1.0],
-    outputRange: ["0%", "100%"],
-    extrapolate: "clamp",
-  });
 
   return (
     <View style={{ ...props.style }}>
-      <FastImage
-        {...props}
-        onProgress={onProgress}
-        onLoad={onLoadStart}
-        onLoadEnd={onLoadEnd}
-      />
-      {showAnimation && (
-        <Animated.View
-          style={[s.progressBar, { transform: [{ scaleX: width }] }]}
-        />
+      <FastImage {...props} onLoadEnd={onLoadEnd} onProgress={onProgress} />
+      {loading && (
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            left: WINDOW_WIDTH / 2 - 110,
+          }}>
+          <Bar progress={progress} width={200} color="green" />
+        </View>
       )}
     </View>
   );
@@ -61,7 +46,7 @@ const s = StyleSheet.create({
     top: 0,
     backgroundColor: "green",
     height: 2,
-    width: 10,
+    width: WINDOW_WIDTH,
   },
 });
 
