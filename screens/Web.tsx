e0f87f38b3d0fc488fe8d29af7812cd25f6eaad2
Aidan Oakley <oaklea@Aidans-MBP.home>
2020-12-25 21:20:09 -0500

    made webview more responsive

--- screens/Web.tsx
+++ screens/Web.tsx
@@ -1,5 +1,13 @@
-import React, { useRef } from "react";
-import { View, Linking, TouchableOpacity } from "react-native";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import {
+  View,
+  Linking,
+  TouchableOpacity,
+  Dimensions,
+  Animated,
+  Easing,
+  StyleSheet,
+} from "react-native";
 import Text from "../components/style/Text";
 import { WebView } from "react-native-webview";
 import { Icon } from "react-native-elements";
@@ -9,8 +17,20 @@ type Props = {
   route: { params: { url: string } };
 };
 
+const width = Dimensions.get("window").width;
+
 const Web: React.FC<Props> = (props) => {
+  const loadAnimation = useRef(new Animated.Value(0)).current;
+  const opacityAnimation = useRef(new Animated.Value(0.5)).current;
   const webRef = useRef<WebView>(null);
+  const [currUrl, setCurrUrl] = useState(props.route.params.url);
+  const [progress, setProgress] = useState(0);
+
+  const onNavigationStateChange = useCallback((navState) => {
+    if (currUrl !== navState.url) {
+      setCurrUrl(navState.url);
+    }
+  }, []);
 
   const goBack = () => {
     if (webRef.current) webRef.current.goBack();
@@ -23,38 +43,112 @@ const Web: React.FC<Props> = (props) => {
   const refresh = () => {
     if (webRef.current) webRef.current.reload();
   };
+
+  const onLoadProgress = (e) => {
+    setProgress(e.nativeEvent.progress);
+  };
+
+  useEffect(() => {
+    Animated.timing(loadAnimation, {
+      toValue: progress,
+      duration: 200,
+      useNativeDriver: true,
+    }).start((e) => {
+      if (e.finished) {
+        if (progress === 1) {
+          Animated.timing(opacityAnimation, {
+            toValue: 1,
+            duration: 300,
+            useNativeDriver: true,
+          }).start((e2) => {
+            if (e2.finished) {
+              Animated.timing(opacityAnimation, {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: true,
+              }).start();
+            }
+          });
+        }
+      }
+    });
+  }, [progress]);
+
+  const interp = loadAnimation.interpolate({
+    inputRange: [0.0, 1.0],
+    outputRange: [0.0, width],
+  });
+
   return (
     <View style={{ flex: 1 }}>
-      {/* SEARCH FUNCTIONS */}
-      <View
-        style={{
-          width: "100%",
-          height: 70,
-          backgroundColor: "black",
-          flexDirection: "row",
-          justifyContent: "space-around",
-          alignItems: "center",
-        }}>
-        <TouchableOpacity onPress={props.navigation.goBack}>
-          <Text>Back</Text>
-        </TouchableOpacity>
-        <Icon name="arrow-back" color="white" onPress={goBack} />
-        <Icon name="arrow-forward" color="white" onPress={goForward} />
-        <Icon name="refresh" color="white" onPress={refresh} />
+      {/* URL AND PROGRESS */}
+      <View style={{ width: "100%", justifyContent: "center" }}>
+        <Animated.View
+          style={[
+            s.loadingIndicator,
+            {
+              opacity: opacityAnimation,
+              transform: [{ translateX: interp }],
+            },
+          ]}
+        />
+        <View
+          style={{ flexDirection: "row", alignItems: "center", height: 50 }}>
+          <TouchableOpacity
+            style={{ margin: 10 }}
+            onPress={props.navigation.goBack}>
+            <Icon name="close" color="white" size={30} />
+          </TouchableOpacity>
+          <Text style={{ color: "grey" }}>{currUrl}</Text>
+        </View>
+      </View>
+      {/* WEB FUNCTIONS */}
+      <View style={s.webFunctions}>
+        <Icon name="arrow-back" color="white" onPress={goBack} size={30} />
+        <Icon
+          name="arrow-forward"
+          color="white"
+          onPress={goForward}
+          size={30}
+        />
+        <Icon name="refresh" color="white" onPress={refresh} size={30} />
         <Icon
           name="link"
           color="white"
           onPress={() => Linking.openURL(props.route.params.url)}
+          size={30}
         />
       </View>
+
       <WebView
         javaScriptCanOpenWindowsAutomatically={false}
         style={{ flex: 1 }}
+        onNavigationStateChange={onNavigationStateChange}
         source={{ uri: props.route.params.url }}
         ref={webRef}
+        onLoadProgress={onLoadProgress}
       />
     </View>
   );
 };
+
+const s = StyleSheet.create({
+  loadingIndicator: {
+    backgroundColor: "rgb(200,200,200)",
+    height: 50,
+    width: width,
+    position: "absolute",
+    bottom: 0,
+    left: -width,
+  },
+
+  webFunctions: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+});
 
 export default Web;
