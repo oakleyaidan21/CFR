11ee4b5d34b32fdbccd7761804eab10d6cda5142
Aidan Oakley <oaklea@Aidans-MBP.home>
2021-01-08 16:10:51 -0500

    changed inbox to flatlist

--- screens/Inbox.tsx
+++ screens/Inbox.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback, useContext, useEffect, useState } from "react";
-import { SectionList, StyleSheet, View } from "react-native";
+import { FlatList, TouchableOpacity, StyleSheet, View } from "react-native";
 import SnooContext from "../context/SnooContext";
 import Text from "../components/style/Text";
 import InboxItem from "../components/InboxItem";
-import { HEADER_HEIGHT } from "../constants/constants";
+import { HEADER_HEIGHT, INBOX_ITEM_HEIGHT } from "../constants/constants";
 
 const Inbox: React.FC = (props) => {
   const { unreadInbox, setUnreadInbox, snoowrap, user } = useContext(
     SnooContext,
   );
 
+  const [showUnread, setShowUnread] = useState(true);
   const [unread, setUnread] = useState(unreadInbox);
   const [read, setRead] = useState([]);
 
@@ -21,54 +22,69 @@ const Inbox: React.FC = (props) => {
     }
   }, []);
 
-  const sections = [
-    {
-      data: unread ? unread : [],
-      renderItem: ({ item }: any) => <InboxItem messageData={item} />,
-      keyExtractor: (item: any) => item.id,
-      title: "Unread Messages",
-    },
-    {
-      data: read,
-      renderItem: ({ item }: any) => <InboxItem messageData={item} read />,
-      keyExtractor: (item: any) => item.id,
-      title: "Read Messages",
-    },
-  ];
-
-  const renderSectionHeader = useCallback((info) => {
+  const renderHeader = useCallback(() => {
     return (
-      <View>
-        <Text style={{ fontWeight: "bold", fontSize: 20 }}>
-          {info.section.title}
-        </Text>
+      <View style={s.headerContainer}>
+        <View style={s.inboxTypeContainer}>
+          <TouchableOpacity onPress={() => setShowUnread(true)}>
+            <Text
+              style={{
+                fontWeight: "bold",
+                color: showUnread ? "green" : "white",
+              }}>
+              Unread
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => setShowUnread(false)}>
+            <Text
+              style={{
+                fontWeight: "bold",
+                color: !showUnread ? "green" : "white",
+              }}>
+              Read
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
-  }, []);
+  }, [showUnread]);
 
-  const renderHeader = useCallback(() => {
-    return <View style={s.headerContainer}></View>;
-  }, []);
+  const renderItem = useCallback(
+    ({ item, index }) => {
+      return <InboxItem messageData={item} read={!showUnread} />;
+    },
+    [showUnread],
+  );
 
+  const getItemLayout = useCallback((data, index) => {
+    return {
+      length: INBOX_ITEM_HEIGHT + 10,
+      offset: (INBOX_ITEM_HEIGHT + 10) * index,
+      index,
+    };
+  }, []);
   return (
     <View style={{ flex: 1, backgroundColor: "rgb(20,20,20)" }}>
       {user ? (
-        <SectionList
-          style={{ flex: 1 }}
-          sections={sections}
-          contentContainerStyle={{ padding: 10 }}
-          renderSectionHeader={renderSectionHeader}
-          ListHeaderComponent={<View style={{ marginTop: HEADER_HEIGHT }} />}
-        />
+        <>
+          <FlatList
+            style={{ flex: 1 }}
+            contentContainerStyle={{ paddingHorizontal: 5 }}
+            data={showUnread ? unread : read}
+            renderItem={renderItem}
+            getItemLayout={getItemLayout}
+            ListHeaderComponent={<View style={{ marginTop: HEADER_HEIGHT }} />}
+          />
+          <View style={{ position: "absolute", top: 0, width: "100%" }}>
+            {renderHeader()}
+          </View>
+        </>
       ) : (
         <View
           style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
           <Text>No user</Text>
         </View>
       )}
-      <View style={{ position: "absolute", top: 0, width: "100%" }}>
-        {renderHeader()}
-      </View>
     </View>
   );
 };
@@ -80,6 +96,14 @@ const s = StyleSheet.create({
     paddingVertical: 5,
     height: HEADER_HEIGHT,
     justifyContent: "flex-end",
+  },
+  inboxTypeContainer: {
+    width: "50%",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: 60,
+    flexDirection: "row",
+    alignSelf: "center",
   },
 });
 
