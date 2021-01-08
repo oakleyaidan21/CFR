ddd5ae11d7ba3f80987bc17f76e7fcc3f7771146
Aidan Oakley <oaklea@Aidans-MBP.home>
2021-01-08 16:28:38 -0500

    can now upvote and downvote

--- components/Score.tsx
+++ components/Score.tsx
@@ -1,39 +1,58 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Text, View } from "react-native";
 import { Icon } from "react-native-elements";
-import { Submission } from "snoowrap";
+import { Comment, Submission } from "snoowrap";
 
 type Props = {
-  data: any;
+  data: Comment | Submission;
   iconSize: number;
+  disabled?: boolean;
 };
 
 const Score: React.FC<Props> = (props) => {
   const { data } = props;
+
+  const [liked, setLiked] = useState(data.likes);
+  const [score, setScore] = useState(data.score);
+
+  const upvote = useCallback(() => {
+    data.upvote();
+    setLiked(liked == true ? null : true);
+    setScore(liked == true ? score - 1 : score + 1);
+  }, [liked, score]);
+
+  const downvote = useCallback(() => {
+    data.downvote();
+    setLiked(liked == false ? null : false);
+    setScore(liked == false ? score + 1 : score - 1);
+  }, [liked, score]);
+
   return (
     <View style={{ flexDirection: "row", alignItems: "center" }}>
-      <Icon
-        name="forward"
-        color="grey"
-        size={props.iconSize}
-        style={{ transform: [{ rotate: "270deg" }] }}
-      />
+      <View style={{ transform: [{ rotate: "270deg" }] }}>
+        <Icon
+          name="forward"
+          color={liked ? "#ff8b5f" : "grey"}
+          size={props.iconSize}
+          onPress={upvote}
+        />
+      </View>
       <Text
         style={{
-          color: "grey",
+          color: liked ? "#ff8b5f" : liked == false ? "#9494ff" : "grey",
           fontWeight: "bold",
           marginHorizontal: 10,
         }}>
-        {data.score > 9999
-          ? (data.score / 1000).toPrecision(3) + "k"
-          : data.score}
+        {score > 9999 ? (score / 1000).toPrecision(3) + "k" : score}
       </Text>
-      <Icon
-        name="forward"
-        color="grey"
-        size={props.iconSize}
-        style={{ transform: [{ rotate: "90deg" }] }}
-      />
+      <View style={{ transform: [{ rotate: "90deg" }] }}>
+        <Icon
+          name="forward"
+          color={liked == false ? "#9494ff" : "grey"}
+          size={props.iconSize}
+          onPress={downvote}
+        />
+      </View>
     </View>
   );
 };
